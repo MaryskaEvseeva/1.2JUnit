ebd08012f27b79ec4e15c9cf275842c2e6ea2fe6
Neto test <sysl86@mail.ru>
2023-09-04 17:21:44 +0400

    commit 3

--- src/test/java/ru/netology/rest/PostmenEchoTest.java
+++ src/test/java/ru/netology/rest/PostmenEchoTest.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmenEchoTest {
     @Test
@@ -17,11 +18,14 @@ public class PostmenEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", egualTo("some data"));
+                .body("data", equalTo("some data"));
            }
 
 
     }
+
+
+
 
 
 
